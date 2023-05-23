2ed45abd1604fd3a1e96200f576034185b342cd3
energy1-914 <dlawldnjs5@daum.net>
2023-05-23 23:20:32 +0900

    feat: add CartItem component

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,20 @@
 export default function Cart() {
+  let items = ["potato", 'tomato', 'coconut']
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem items = {items[0]}/>
+      <CartItem items = {items[1]}/>
     </div>
   );
+}
+
+function CartItem(props) {
+  return(
+    <div className="cart-item">
+        <p>상품명 {props.items}</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+  )
 }
